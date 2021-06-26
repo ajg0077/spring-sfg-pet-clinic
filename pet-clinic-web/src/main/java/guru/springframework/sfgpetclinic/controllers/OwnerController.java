ecb88b73fcb6af9354b3895cb789b488fc474418
Antony <ajg0077@gmail.com>
2021-06-26 17:24:06 +0530

    Create Owner Index Page and Controllers - Closes #14

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -2,12 +2,12 @@ package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"","/","/index","/index.html"})
     public String listVets(){
-        return "vets/index";
+        return "owners/index";
     }
 }
